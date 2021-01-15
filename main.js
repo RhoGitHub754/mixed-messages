61d22e7d6b1e67be42252f514372e240ae0ebb55
Rhoan Sesay <rhoansesay@Rhoans-MacBook-Pro.local>
2021-01-15 20:13:47 +0000

    added quotes

---
+++ main.js
@@ -0,0 +1,2 @@
+const messageType = ['Health', 'Wealth', 'Love', 'Spirituality'];
+const quotes = ['There is only one happiness in this life, to love and be loved. - George Sand', 'I swear I couldnt love you more than I do right now, and yet I know I will tomorrow. - Leo Christopher', 'Love does not dominate; it cultivates. - Johann Wolfgang von Goethe', 'The only thing we never get enough of is love; and the only thing we never give enough of is love. - Henry Miller']
